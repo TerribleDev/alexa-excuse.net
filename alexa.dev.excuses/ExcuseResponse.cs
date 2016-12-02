6ae2fe089f6dd4d41ac70e941c73f10207bff81c
Tommy Parnell <tparnell8@gmail.com>
2016-12-02 15:52:48 -0500

    use a static list, no http calls

--- alexa.dev.excuses/ExcuseResponse.cs
+++ alexa.dev.excuses/ExcuseResponse.cs
@@ -7,7 +7,6 @@ using AlexaSkillsKit.Authentication;
 using AlexaSkillsKit.Json;
 using System.Threading.Tasks;
 using AlexaSkillsKit.UI;
-using AngleSharp;
 
 namespace alexa.dev.excuses
 {
@@ -46,32 +45,8 @@ namespace alexa.dev.excuses
 		public static async Task<string> GetExcuses()
 		{
 			// Setup the configuration to support document loading
-			var config = Configuration.Default.WithDefaultLoader();
-			// Load the names of all The Big Bang Theory episodes from Wikipedia
-			var rando = new Random().Next(0,2);
-			string address = null;
-			if (rando == 0)
-			{
-				address = "http://programmingexcuses.com/";
-			}
-			if(rando == 1)
-			{
-				address = "http://www.devexcuses.com/";
-			}
-			if(address == null)
-			{
-				throw new Exception("Something really wrong with the rando generator");
-			}
-			// Asynchronously get the document in a new context using the configuration
-			using (var document = await BrowsingContext.New(config).OpenAsync(address))
-			{
-				// This CSS selector gets the desired content
-				var cellSelector = "a";
-				// Perform the query to get all cells with the content
-				var cells = document.QuerySelectorAll(cellSelector);
-				// We are only interested in the text - select it with LINQ
-				return cells.First().TextContent;
-			}
+			var item = new Random().Next(0, Excuses.ExcuseList.Count);
+			return Excuses.ExcuseList[item];
 		}
 	}
 }
